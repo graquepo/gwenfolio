bf0cb2b2dbfaa36550d271d5c3e492533f043ca1
graquepo <graquepo@hawaii.edu>
2025-03-03 11:21:10 -1000

    update transition

--- js/custom.js
+++ js/custom.js
@@ -9,11 +9,36 @@ function fadeIn(){
 
 }
 
+// document.addEventListener('DOMContentLoaded', () => {
+//     const content = document.getElementById('content');
+
+//     function loadPage(url, transitionClass = 'hidden-fade-slide') {
+//         content.classList.add(transitionClass);
+//         setTimeout(() => {
+//             fetch(url)
+//                 .then(response => response.text())
+//                 .then(html => {
+//                     content.innerHTML = html;
+//                     content.className = '';
+//                 });
+//         }, 500);
+//     }
+
+//     document.querySelectorAll('a').forEach(link => {
+//         link.addEventListener('click', event => {
+//             event.preventDefault();
+//             const url = link.getAttribute('href');
+//             const transitionClass = link.dataset.transition || 'hidden-fade-slide';
+//             loadPage(url, transitionClass);
+//         });
+//     });
+// });
+
 document.addEventListener('DOMContentLoaded', () => {
     const content = document.getElementById('content');
 
-    function loadPage(url, transitionClass = 'hidden-fade-slide') {
-        content.classList.add(transitionClass);
+    function loadPage(url, direction = 'right') {
+        content.classList.add(`hidden-${direction}`);
         setTimeout(() => {
             fetch(url)
                 .then(response => response.text())
@@ -28,8 +53,8 @@ document.addEventListener('DOMContentLoaded', () => {
         link.addEventListener('click', event => {
             event.preventDefault();
             const url = link.getAttribute('href');
-            const transitionClass = link.dataset.transition || 'hidden-fade-slide';
-            loadPage(url, transitionClass);
+            const direction = link.dataset.direction || 'right';
+            loadPage(url, direction);
         });
     });
 });
