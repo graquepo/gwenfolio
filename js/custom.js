29710f90c782fac49a5a8f1013b7eb9776fb994c
graquepo <graquepo@hawaii.edu>
2025-03-03 13:04:13 -1000

    fix translation

--- js/custom.js
+++ js/custom.js
@@ -9,11 +9,36 @@ function fadeIn(){
 
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+    const content = document.getElementById('content');
+
+    function loadPage(url, transitionClass = 'fading') {
+        content.classList.add(transitionClass);
+        setTimeout(() => {
+            fetch(url)
+                .then(response => response.text())
+                .then(html => {
+                    content.innerHTML = html;
+                    content.className = '';
+                });
+        }, 500);
+    }
+
+    document.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault();
+            const url = link.getAttribute('href');
+            const transitionClass = link.dataset.transition || 'fading';
+            loadPage(url, transitionClass);
+        });
+    });
+});
+
 // document.addEventListener('DOMContentLoaded', () => {
 //     const content = document.getElementById('content');
 
-//     function loadPage(url, transitionClass = 'hidden-fade-slide') {
-//         content.classList.add(transitionClass);
+//     function loadPage(url, direction = 'left') {
+//         content.classList.add(`hidden-${direction}`);
 //         setTimeout(() => {
 //             fetch(url)
 //                 .then(response => response.text())
@@ -28,33 +53,8 @@ function fadeIn(){
 //         link.addEventListener('click', event => {
 //             event.preventDefault();
 //             const url = link.getAttribute('href');
-//             const transitionClass = link.dataset.transition || 'hidden-fade-slide';
-//             loadPage(url, transitionClass);
+//             const direction = link.dataset.direction || 'right';
+//             loadPage(url, direction);
 //         });
 //     });
 // });
-
-document.addEventListener('DOMContentLoaded', () => {
-    const content = document.getElementById('content');
-
-    function loadPage(url, direction = 'left') {
-        content.classList.add(`hidden-${direction}`);
-        setTimeout(() => {
-            fetch(url)
-                .then(response => response.text())
-                .then(html => {
-                    content.innerHTML = html;
-                    content.className = '';
-                });
-        }, 3000);
-    }
-
-    document.querySelectorAll('a').forEach(link => {
-        link.addEventListener('click', event => {
-            event.preventDefault();
-            const url = link.getAttribute('href');
-            const direction = link.dataset.direction || 'right';
-            loadPage(url, direction);
-        });
-    });
-});
